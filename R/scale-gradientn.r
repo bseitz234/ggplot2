6d694589a44f7d9da05298ec1d042fa3e37a0da6
Brian C. Seitz <bseitz234@gmail.com>
2015-09-24 13:48:31 -0400

    Added ability for scale-gradientn to accept "color" spelling of colour as an argument.

--- R/scale-gradientn.r
+++ R/scale-gradientn.r
@@ -2,13 +2,15 @@
 #' @param colours Vector of colours to use for n-colour gradient.
 #' @rdname scale_gradient
 #' @export
-scale_colour_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar") {
+scale_colour_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar", color = NULL) {
+  if(!is.null(color)) colour <- color
   continuous_scale("colour", "gradientn",
     gradient_n_pal(colours, values, space), na.value = na.value, guide = guide, ...)
 }
 #' @rdname scale_gradient
 #' @export
-scale_fill_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar") {
+scale_fill_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar", color = NULL) {
+  if(!is.null(color)) colour <- color
   continuous_scale("fill", "gradientn",
     gradient_n_pal(colours, values, space), na.value = na.value, guide = guide, ...)
 }
