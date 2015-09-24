a6502b7ef55685435e44a40f649d52cf20864eb5
Brian C. Seitz <bseitz234@gmail.com>
2015-09-24 13:51:26 -0400

    Replaced "color" with "colors" for consistency with existing "colours" argument.

--- R/scale-gradientn.r
+++ R/scale-gradientn.r
@@ -2,15 +2,15 @@
 #' @param colours Vector of colours to use for n-colour gradient.
 #' @rdname scale_gradient
 #' @export
-scale_colour_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar", color = NULL) {
-  if(!is.null(color)) colour <- color
+scale_colour_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar", colors = NULL) {
+  if(!is.null(colors)) colours <- colors
   continuous_scale("colour", "gradientn",
     gradient_n_pal(colours, values, space), na.value = na.value, guide = guide, ...)
 }
 #' @rdname scale_gradient
 #' @export
-scale_fill_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar", color = NULL) {
-  if(!is.null(color)) colour <- color
+scale_fill_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar", colors = NULL) {
+  if(!is.null(colors)) colours <- colors
   continuous_scale("fill", "gradientn",
     gradient_n_pal(colours, values, space), na.value = na.value, guide = guide, ...)
 }
